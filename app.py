1a4de7496c80c83146ee42b92c2f324790873de0
seulah <leeseula0123@gmail.com>
2024-09-23 23:39:47 +0900

    feat : flask 서버 구축

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import io
 from ultralytics import YOLO
 
 # YOLO 모델 가져오기
-model = YOLO('/Users/iseul-a/Downloads/helmet_result/best-2.pt')
+model = YOLO('/Users/iseul-a/Downloads/아카이브/best-2.pt')
 app = Flask(__name__)
+
 @app.route('/process-image', methods=['POST'])
 def process_image():
     if 'image' not in request.files:
@@ -18,12 +19,23 @@ def process_image():
     img = Image.open(io.BytesIO(image_bytes))
 
     # YOLO 모델로 이미지 처리
-    results = model(img)
+    results = model(img, conf=0.25)  # 임계값 조정
 
-    # 결과를 딕셔너리로 변환 (YOLO 모델의 results를 처리)
-    processed_data = results.pandas().xyxy[0].to_dict(orient="records")
+    # 라벨링 정보만 추출
+    labels = []
+    for result in results:
+        for i in range(len(result.boxes.cls)):  # 감지된 객체 수만큼 반복
+            class_id = int(result.boxes.cls[i])  # 클래스 ID 가져오기
 
-    return jsonify({"detections": processed_data})
+            # 헬멧 착용 여부에 따라 라벨링
+            if class_id == 0:
+                label = "Helmet Worn"
+            elif class_id == 1:
+                label = "No Helmet"
+            labels.append(label)
+
+    # 감지된 라벨 리스트를 JSON으로 반환
+    return jsonify({"labels": labels})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
