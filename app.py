ffc643e32fd3f08c934838c53abfaf19d7119418
seulah <leeseula0123@gmail.com>
2024-10-08 12:37:59 +0900

    feat : 사진 업로드 후 페이지 이동

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import torch
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify , render_template_string
 from PIL import Image
 import io
 from ultralytics import YOLO
@@ -29,7 +29,20 @@ def process_image():
 
             # 헬멧 착용 여부에 따라 라벨링
             if class_id == 0:
-                label = "Helmet Worn"
+                # 직접 HTML을 반환하여 클라이언트 측에서 페이지 이동 처리
+                return render_template_string('''
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <title>Redirecting...</title>
+                    <script>
+                        window.location.href = "http://localhost:8080/success";
+                    </script>
+                </head>
+                <body>
+                </body>
+                </html>
+                ''')
             elif class_id == 1:
                 label = "No Helmet"
             labels.append(label)
